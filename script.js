3f44c1046217b8af17bcbb93f3591c50dab7f3e9
Rajdeep Mitra <116889292+mitrarajdeep2001@users.noreply.github.com>
2023-04-21 11:37:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let line = document.querySelector('#line');
+let angle = 0;
+function rotateLine() {
+	angle++;
+	line.style.transform = 'rotate('+ angle +'deg)';
+	requestAnimationFrame(rotateLine);
+}
+rotateLine();
